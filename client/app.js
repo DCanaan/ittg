bb0e191612194709bde92b784a95926cf9aa812b
Danny Canaan <danny.canaan@gmail.com>
2015-11-03 23:13:34 +0100

    Ajout login

--- client/app.js
+++ client/app.js
@@ -1,3 +1,27 @@
 Template.messages.helpers({
   messages: Messages.find({})
 });
+
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+})
+
+Template.registerHelper("timestampToTime", function (timestamp) {
+	var date = new Date(timestamp);
+	var hours = date.getHours();
+	var minutes = "0" + date.getMinutes();
+	var seconds = "0" + date.getSeconds();
+	return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
+});
+
+Template.registerHelper("usernameFromId", function (userId) {
+	var user = Meteor.users.findOne({_id: userId});
+	console.log(user.username);
+	if (typeof user === "undefined") {
+		return "Anonymous";
+	}
+	return user.username;
+});
+
+Meteor.subscribe('messages');
+Meteor.subscribe('allUsernames');
